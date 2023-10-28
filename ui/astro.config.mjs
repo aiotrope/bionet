a6b6173bf23a850ac3c11aae4d637ea2a4e25f10
aiotrope <arneli74@protonmail.com>
2023-10-28 19:12:13 +0300

    ♻️ refactor(ui): add ui directory

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  server: {
+    port: 4321,
+    host: true,
+  },
 });
